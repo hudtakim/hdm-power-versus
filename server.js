97609fa6a53290ee06acc08b937c9b926ec701f4
unknown <hudtakim@gmail.com>
2025-08-05 15:51:04 +0700

    init

--- server.js
+++ server.js
@@ -62,20 +62,20 @@ io.on('connection', (socket) => {
         player.tapCount++;
 
         if (player.role === 'Player 1') {
-            rooms[room].pos += (player.step - opponent.mass);
+            rooms[room].pos += (player.step - opponent.mass)/2;
         } else {
-            rooms[room].pos -= (player.step - opponent.mass);
+            rooms[room].pos -= (player.step - opponent.mass)/2;
         }
         
         if (rooms[room].pos > 0 && rooms[room].pos < 100) {
             let massScale = opponent.mass > 0 ? (player.mass + 1) / (opponent.mass + 1) : player.mass;
             const stepScale = player.step / opponent.step;
-            opponent.boost += (((player.step - opponent.mass) * massScale * stepScale) * 0.03);
+            const posScale = player.role === 'Player 1' ? rooms[room].pos / 100 : (100 - rooms[room].pos) / 100;
+            opponent.boost += ((player.step - opponent.mass) * massScale * stepScale * posScale * 0.05);
             //if(opponent.boost < 1) opponent.boost = 1;
 
             if(player.boostState && (player.step > baseStep + player.scaler)){ //reduce to its normal step
                 //player.step -= (((player.role === 'Player 1' ? rooms[room].pos / 100 : (100 - rooms[room].pos) / 100) * player.boost));
-                let posScale = player.role === 'Player 1' ? rooms[room].pos / 100 : (100 - rooms[room].pos) / 100;
                 player.step -= ((player.step - (baseStep + player.scaler)) * posScale);  
                 player.boost = player.step / (baseStep + player.scaler);
                 if(player.boost < 1) player.boost = 1;
